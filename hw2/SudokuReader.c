3841d0f65133210cb4db87a61c0bb0a5799fcf03
sferguson670 <sferguson670@gmail.com>
2018-09-23 10:49:14 -0600

    add methods
    Add checking methods and file scanner

--- hw2/SudokuReader.c
+++ hw2/SudokuReader.c
@@ -1,6 +1,7 @@
 /**
  * A program that will take a .txt input and will determine
  * whether or not the Sudoku puzzle is valid.
+ * Uses multi-threading.
  *
  * Sarah Ferguson
  * Sept. 2018
@@ -13,15 +14,45 @@
 #include <stdlib.h>
 
 #define NUM_CHILD_THREADS 27
+#define TRUE 1
+#define FALSE 0
 
-int sum[NUM_CHILD_THREADS]; /* this data is shared by the thread(s) */
+typedef int bool;
+
+/* global variables */
+int sum[NUM_CHILD_THREADS];
+int sudokuPuzzle[9][9];
+int tid_columns[9]; /* thread IDs for columns, rows, and subgrids */
+int tid_rows[9];
+int tid_subgrids[9];
+bool columns[9]; /* boolean arrays for columns, rows, and subgrids */
+bool rows[9];
+bool subgrids[9];
 
 void *runner(void *param); /* the thread */
 
+/* represents the index (row and column) range */
 typedef struct {
-    int upper;
-    int iValue;
+    int topRow;
+    int bottomRow;
+    int leftColumn;
+    int rightColumn;
 } run_param;
+
+/* checks the columns */
+void *checkColumn (void* param) {
+
+}
+
+/* checks the rows */
+void *checkRow (void* param) {
+
+}
+
+/* checks subgrids 3x3 */
+void *checkSubGrid (void* param) {
+
+}
 
 int main(int argc, char *argv[]) {
     /* opens up .txt file */
@@ -33,72 +64,22 @@ int main(int argc, char *argv[]) {
         printf("Error. Could not open file");
         /* Program exits if the file pointer returns NULL. */
         exit(1);
+    } else {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (fscanf(ptr_file, "%d", &num) != 1)
+                    printf("Error");
+                else
+                    sudokuPuzzle[i][j] = num;
+            }
+        }
     }
-
-    fscanf(ptr_file, "%d", % num);
-    printf("Value of n=%d", num);
     fclose(ptr_file);
 
-    int i;
-    pthread_t tid[NUM_CHILD_THREADS]; /* the thread identifier for child threads */
-    pthread_attr_t attr[NUM_CHILD_THREADS]; /* set of attributes for the thread */
-    run_param thrParam[NUM_CHILD_THREADS];
-
-    if (argc != 2) {
-        fprintf(stderr, "usage: thrd-posix <0 or positive integer value>\n");
-        /*exit(1);*/
-        return -1;
+    /* print outs the sudoku puzzle */
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            printf("%d\t", sudokuPuzzle[i][j]);
+        }
     }
-
-    if (atoi(argv[1]) < 0) {
-        fprintf(stderr, "Argument %d must be non-negative\n", atoi(argv[1]));
-        /*exit(1);*/
-        return -1;
-    }
-
-    /* get the >= value for upper */
-    for (i = 0; i < NUM_CHILD_THREADS; i++) {
-        thrParam[i].upper = atoi(argv[1]);
-        thrParam[i].iValue = i;
-    }
-
-    /* get the default attributes */
-    for (i = 0; i < NUM_CHILD_THREADS; i++)
-        pthread_attr_init(&(attr[i]));
-
-    /* create the threads */
-    for (i = 0; i < NUM_CHILD_THREADS; i++)
-        pthread_create(&(tid[i]), &(attr[i]), runner, &(thrParam[i]));
-
-    /* now wait for the thread to exit */
-    for (i = 0; i < NUM_CHILD_THREADS; i++)
-        pthread_join(tid[i], NULL);
-
-    for (i = 0; i < NUM_CHILD_THREADS; i++)
-        printf("sum[%d] = %d\n", i, sum[i]);
-
-}
-
-/* The thread will begin control in this function */
-void *runner(void *param) {
-    run_param *inP;
-    int i, up;
-    int iV;
-    pthread_t self;
-
-    inP = (run_param *)param;
-    up = inP->upper;
-    iV = inP->iValue;
-
-    sum[iV] = 0;
-
-    if (up > 0) {
-        for (i = 1; i <= up; i++)
-            sum[iV] += i;
-    }
-
-    self = pthread_self();
-    printf("ending child thread #%d (tid = %1X)\n", iV, (unsigned long) self);
-
-    pthread_exit(0);
 }
